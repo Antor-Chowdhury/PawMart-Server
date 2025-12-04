0fe285f2e301e6a7a66c9e1f54a99478694cd709
Antor-Chowdhury <antor1347@gmail.com>
2025-12-05 01:41:55 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
+
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
